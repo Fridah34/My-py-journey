b51203dcbfc5d408857f235e94b4edc285c6e635
Fridah Nthambi <fridahnthambi69@gmail.com>
2025-01-15 16:55:11 +0300

    py journey

---
+++ Typecasting.py
@@ -0,0 +1,9 @@
+#typecasting = process of converting a variable from one data type to another str(), int(), float(), bool()
+from variables import is_student
+
+name ="Fridah Nthambi"
+age = 20
+gpa =3.5
+is_student =  True
+
+print(type(name))
---
+++ variables.py
@@ -0,0 +1,42 @@
+#variable=A container for a value (string,intger,float,boolean)
+
+#Strings
+first_name= "Nthambi"
+food= "Hotdog"
+
+print(f"Hello {first_name}")
+print(f"I love {food}")
+#f stands for format
+
+#Integer
+age =25
+quantity = 3
+num_of_students = 43
+
+print(f"I am {age} years old")
+print(f"I am buying {quantity} dresses")
+print(f"Our class has {num_of_students} students")
+
+#float
+price = 10.50
+gpa = 3.2
+
+print(f"The price is ${price}")
+print(f"Your gpa is: {gpa}")
+
+#boolen = either true or false
+
+is_student = False
+is_pretty = True
+
+if is_student:
+    print("He is a student")
+else:
+    print("He is not a student")
+
+if is_pretty:
+    print("She is pretty")
+else:
+    print("She is not attractive")
+
+
